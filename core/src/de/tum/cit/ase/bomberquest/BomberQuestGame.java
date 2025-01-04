06409a2644abc9079dd0ffbc885afd54eaa51c06
Tobias <tobias-horn@posteo.de>
2025-01-04 14:25:51 +0100

    Removed map creation from the BomberQuestGame.java file to add at a later stage in the program flow

--- core/src/de/tum/cit/ase/bomberquest/BomberQuestGame.java
+++ core/src/de/tum/cit/ase/bomberquest/BomberQuestGame.java
@@ -3,6 +3,7 @@ package de.tum.cit.ase.bomberquest;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import de.tum.cit.ase.bomberquest.audio.MusicTrack;
@@ -59,7 +60,6 @@ public class BomberQuestGame extends Game {
     public void create() {
         this.spriteBatch = new SpriteBatch(); // Create SpriteBatch for rendering
         this.skin = new Skin(Gdx.files.internal("skin/craftacular/craftacular-ui.json")); // Load UI skin
-        this.map = new GameMap(this); // Create a new game map (you should change this to load the map from a file instead)
         MusicTrack.BACKGROUND.play(); // Play some background music
         goToMenu(); // Navigate to the menu screen
     }
